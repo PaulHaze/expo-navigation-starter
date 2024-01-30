e18d50eab1a1a665f923aa00d0ba4f6cea5795c4
PaulHaze <dev@artemist.com.au>
2024-01-30 16:23:42 +1100

    add a first navigator

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,11 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { HomeScreen } from '@/screens/Home';
+
+const HomeStack = createNativeStackNavigator();
+const HomeStackScreens = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={HomeScreen} />
+  </HomeStack.Navigator>
+);
 
 export function MainNavigator() {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <HomeStackScreens />
     </NavigationContainer>
   );
 }
