77fe239fa9bd77692f4f1218a8162feb201cc165
PaulHaze <dev@artemist.com.au>
2024-01-30 12:45:23 +1100

    eslint config updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,17 +36,16 @@ module.exports = {
         tsconfigRootDir: __dirname,
       },
       rules: {
-        '@typescript-eslint/no-use-before-define': 'off',
+        // NATIVE SPECIFIC
+        'react-native/sort-styles': 'off',
         'react/jsx-filename-extension': [0],
         'no-undef': 'error',
         'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
-        'jsx-a11y/anchor-is-valid': 'off', // Next.js uses its own internal link system
         'react/require-default-props': 'off', // Allow non-defined react props as undefined
         'react/jsx-props-no-spreading': 'off', // _app.tsx uses spread operator and also, react-hook-form
-        'react-hooks/exhaustive-deps': 'off', // Incorrectly report needed dependency with Next.js router
-        '@next/next/no-img-element': 'off', // We currently not using next/image because it isn't supported with SSG mode
         '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between Eslint and Prettier
         '@typescript-eslint/consistent-type-imports': 'error', // Ensure  is used when it's necessary
+        '@typescript-eslint/no-use-before-define': 'off',
         'import/prefer-default-export': 'off', // Named export is easier to refactor automatically
         'import/extensions': [
           'warn',
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/sort-styles */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,8 +7,10 @@ import { BASE } from '@/styles/globalStyles';
 export function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>Bare Bones Home Screen</Text>
-      <Text>Delete this and start building</Text>
+      <Text style={[styles.headerText, styles.textColor]}>
+        Bare Bones Home Screen
+      </Text>
+      <Text style={styles.textColor}>Delete this and start building</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,9 +19,12 @@ export function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    backgroundColor: BASE.COLORS.WHITE,
+    backgroundColor: BASE.COLORS.BLACK,
     flex: 1,
     justifyContent: 'center',
+  },
+  textColor: {
+    color: BASE.COLORS.WHITE,
   },
   headerText: {
     fontSize: 24,
